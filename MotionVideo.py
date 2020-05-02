3ec9c372560ef5318ebcbe2f8633572f5f66a08f
Narayana Raj <48882141+narayanarajvj@users.noreply.github.com>
2020-05-02 21:48:49 +0530

    Update MotionVideo.py

--- MotionVideo.py
+++ MotionVideo.py
@@ -8,16 +8,21 @@ while True:
     a = a + 1
     check, frame = video.read()
     # print(frame)
+    
+    # Font type to be used
     font = cv2.FONT_HERSHEY_SIMPLEX
+    
+    # To display text on video
     cv2.putText(frame,
                 'Press \'q\' to quit',
                 (50, 50),
                 font, 1,
                 (0, 255, 255),
                 2)
-
+    # For Color video
     cv2.imshow('My_Color_Video', frame)
-
+    
+    # For Black and White / Gray  video
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('My_Video', gray)
 
